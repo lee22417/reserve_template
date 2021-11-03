236309a2396abda1b6631f56d54cc1236caa478b
jiwon <jlee22417@gmail.com>
2021-11-03 18:09:28 +0900

    create reservation dto

--- src/reservation/dto/create-reservation.dto.ts
+++ src/reservation/dto/create-reservation.dto.ts
@@ -1,1 +1,12 @@
-export class CreateReservationDto {}
+import { IsDate, IsNumber, IsString } from 'class-validator';
+
+export class CreateReservationDto {
+  @IsDate()
+  reserved_at: Date;
+
+  @IsNumber()
+  num_of_people: number;
+
+  @IsNumber()
+  price: number;
+}
