b03e9b86c01d1d37c4d1110d514f382ced5160d2
jiwon <jlee22417@gmail.com>
2021-11-08 16:39:19 +0900

    create reservation find functions

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Reservation } from 'src/entities/reservation.entity';
+import { Repository } from 'typeorm';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
 
 @Injectable()
 export class ReservationService {
+  constructor(
+    @InjectRepository(Reservation)
+    private rsvRepository: Repository<Reservation>,
+  ) {}
+
   create(createReservationDto: CreateReservationDto) {
     return 'This action adds a new reservation';
   }
 
-  findAll() {
-    return `This action returns all reservation`;
+  async findAll() {
+    return await this.rsvRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} reservation`;
+  // find by pk
+  async findOne(no: number) {
+    return await this.rsvRepository.findOne({ no: no });
   }
+
+  //TODO find by user id, reservation date
 
   update(id: number, updateReservationDto: UpdateReservationDto) {
     return `This action updates a #${id} reservation`;
