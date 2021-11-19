3e7ccafa0324e773cc686434669d09444de34577
jiwon <jlee22417@gmail.com>
2021-11-19 12:05:43 +0900

    get auth middleware setting

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,8 +1,17 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('Middleware');
+    const token = req.header('authorization');
+
+    if (token) {
+      // const payload = [];
+      // console.log(payload);
+      // res.locals.payload = payload;
+    }
     next();
   }
 }
