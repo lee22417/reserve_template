62e722e8dc69d7ad3e49bb62240ef6a2f3ffeadd
jiwon <jlee22417@gmail.com>
2022-03-03 11:44:45 +0900

    document swagger

--- src/api/reservation/dto/create-reservation.dto.ts
+++ src/api/reservation/dto/create-reservation.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateReservationDto {
   @IsNotEmpty()
+  @ApiProperty({ description: '예약 일자 및 시간', example: '2020-02-02' })
   reserved_at;
 
   @IsNumber()
+  @ApiProperty({ description: '예약 인원 수', example: '1' })
   num_of_people: number;
 
   @IsNumber()
+  @ApiProperty({ description: '예약 가격', example: '10000' })
   price: number;
 }
--- src/api/user/dto/create-user.dto.ts
+++ src/api/user/dto/create-user.dto.ts
@@ -1,18 +1,24 @@
-import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @ApiProperty({ description: '회원 ID', example: 'test' })
   id: string;
 
   @IsString()
+  @ApiProperty({ description: '회원 이름', example: 'name' })
   name: string;
 
   @IsString()
+  @ApiProperty({ description: '회원 비밀번호', example: '123123' })
   password: string;
 
   @IsString()
+  @ApiProperty({ description: '전화 번호', example: '010-1234-1234' })
   phone_number: string;
 
-  @IsString()
+  @IsEmail()
+  @ApiProperty({ description: '이메일', example: 'test@email.com' })
   email: string;
 }
